f6dc17e27d609ced6dbf62d7456b62357b60be6d
RJVG <vil15321@uvg.edu.gt>
2016-07-16 14:07:22 -0600

    Add files via upload

--- RadioMain.java
+++ RadioMain.java
@@ -64,15 +64,79 @@ public class RadioMain implements Radio{
             return am;
         }
     }
-    public void changeFrequency(String estacion){
-        if (estacion.equals ("FM")){
+    public void changeFrequency(String fre){
+        if (fre.equals ("FM")){
             this.FM = true;
-        }else if (estacion.equals ("AM")){
+        }else if (fre.equals ("AM")){
             this.FM = false;
         }else {
             System.out.println("Error de ingreso");
         }
     }
-
+    public String getStation(){
+        String station = Double.toString(estacion);
+        return station;
+    }
+    public void setStation(){
+        switch(this.boton_elegido){
+            case 1:
+                this.b1 = estacion;
+            case 2:
+                this.b2 = estacion;
+            case 3:
+                this.b3 = estacion;
+            case 4:
+                this.b4 = estacion;
+            case 5:
+                this.b5 = estacion;
+            case 6:
+                this.b6 = estacion;
+            case 7:
+                this.b7 = estacion;
+            case 8: 
+                this.b8 = estacion;
+            case 9:
+                this.b9 = estacion;
+            case 10:
+                this.b10 = estacion;
+            case 11:
+                this.b11 = estacion;
+            case 12:
+                this.b12 = estacion;
+        }
+    }
+    public void Forward(){
+        if (FM){
+            if(this.estacion < 107.9){
+                this.estacion = this.estacion + 0.2;
+            } else if (this.estacion == 107.9){
+                this.estacion = 87.9;
+            }
+        }else {
+            if (this.estacion < 1610){
+                this.estacion = this.estacion + 10;
+            }else if(this.estacion == 1610){
+                this.estacion = 530;
+            }
+        }
+    }
+    public void Backward(){
+        if (FM){
+            if (this.estacion > 87.9){
+                this.estacion = this.estacion - 0.2;
+            }else if (this.estacion == 87.9){
+                this.estacion = 107.9;
+            }
+        }else{
+            if (this.estacion > 530){
+                this.estacion = this.estacion -10;
+            }else if (this.estacion == 530){
+                this.estacion = 1610;
+            }
+        }
+    }
+    public void setMemory(String station, int position, String fre){
+        throw new UnsupportedOperationException ("Not supported yet.");        
+    }
 }
 
